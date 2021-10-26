1cf91258adf146451e6f188c8e359ea10b8e7ba8
pd19ms <93050683+pd19ms@users.noreply.github.com>
2021-10-26 20:54:39 +0530

    Update login.c

--- login.c
+++ login.c
@@ -1,11 +1,18 @@
 // PRIYANGSHU DATTA 19MS033
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <time.h>
 #ifdef _WIN32
+    #include <stdio.h>
+    #include <string.h>
+    #include <stdlib.h>
+    #include <time.h>
     #include <conio.h>
+#elif linux
+    #include <stdio.h>
+    #include <string.h>
+    #include <stdlib.h>
+    #include <time.h>
+    #include <termios.h>
+    #include <unistd.h>
 #endif
 
 typedef struct{
@@ -32,7 +39,7 @@ void booking();
 long int gen_ticket();
 void check();
 
-const char* getpass(){
+const char* getpasswd(){
     char *passwrd;
     char ch;
     #ifdef _WIN32
@@ -116,7 +123,7 @@ void login(int x){
     pass:
     if(!flag){
         printf("Please enter your password: ");
-        strcpy(temp.pass,getpass());
+        strcpy(temp.pass,getpasswd());
         goto l3;
     }
     int logged = 1;
@@ -249,9 +256,9 @@ void userSignup(){
     }
     pass:
     printf("Enter a strong password: ");
-    const char *pass = getpass();
+    const char *pass = getpasswd();
     printf("\nEnter the password to confirm: ");
-    const char *cpass = getpass();
+    const char *cpass = getpasswd();
     if (strcmp(pass,cpass)==0){
         person temp;
         strcpy(temp.uname,uname);
@@ -336,7 +343,7 @@ void booking(person *user){
             printf("\nInformation regarding flights (search flights).");
             printf("Do you want to continue? Press enter to continue, carraige return to quit.\n");
             char ch;
-            ch = getch();
+            ch = getchar();
             switch(ch){
                 case 13:
                     break;
