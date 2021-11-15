f9d821279a0b4a68eca0024c01d3bc5f96f66378
pd19ms <93050683+pd19ms@users.noreply.github.com>
2021-11-15 19:36:14 +0530

    admin flight add and read

--- login.c
+++ login.c
@@ -31,7 +31,8 @@ typedef struct{
     int seats;
     char time[10];
     char source[20];
-    char destinantion[20];
+    char destination[20];
+    char name[20];
 }flight;
 
 int menu();
@@ -375,9 +376,11 @@ void booking(person *user){
             printf("\nYour prefered choice of travel date is %d %s %d. Enter 0 to confirm, 1 to change date: ", d,months[m-1],y);
             scanf("%d",&flag);
         }
-        strcpy(t.f_dt,itoa(d,dt,10));
+        itoa(d,dt,10);
+        itoa(y,yr,10);
+        strcpy(t.f_dt,dt);
         strcpy(t.f_mt,months[m-1]);
-        strcpy(t.f_yr,itoa(d,yr,10));
+        strcpy(t.f_yr,yr);
         break;
     case 2:
         // flightInfo();
@@ -519,7 +522,7 @@ void admin(){
             signup(1);
             break;
         case 2:
-            // flights();
+            flights();
             break;
         case 3:
             // user_deletion();
@@ -534,13 +537,103 @@ void admin(){
 }
 
 void flights(){
+	int choice=0;
+    char id[10];
+	flight fl,temp;
+	while(choice==0){
     printf("Enter 1 for add flight, 2 for delete flight, 3 for modify flight: ");
+    scanf("%d",&choice);
+    FILE *file;
+	file = fopen("flights.dat","a+"); 
+	switch (choice){
+		case 1: //add flights
+			zero:
+			printf("\nEnter the source of flight : ");
+			scanf("%s",fl.source);
+			printf("\nEnter the destination of flight : ");
+			scanf("%s",fl.destination);
+			printf("\nEnter the name of the flight : ");
+			scanf("%s",fl.name);
+			printf("Source : %s\nDestination:%s\nFlight name:%s\n",fl.source,fl.destination,fl.name);
+			one:
+			printf("\nEnter 1 to Confirm and 2 to Change: ");
+			int flag=0;
+			scanf("%d",&flag);
+			switch(flag){
+				case 1: break;
+				case 2: goto zero;
+				break;
+				default:printf("Enter a valid option!"); goto one;
+				break;	
+			}
+			int i=0;
+			for(i=0;i<3;i++){
+				fl.id[i]=fl.name[i];
+			}
+			char num[20];
+			int ch1 = fl.source[0],ch2=fl.destination[0];
+			int ch3 = ch1*100+ch2;
+			itoa(ch3,num,10);
+			strcat(fl.id,num);
+            while(fread(&temp,sizeof(flight),1,file)==1){
+                if (strcmp(fl.id,temp.id)==0){
+                    printf("Error! Flight with same ID exists.");
+                    goto zero;
+                    break;
+                }
+            }
+			printf("\nEnter flight time : ");
+			int h,m; char th[10],tm[10];
+            printf("\nEnter hours: ");
+            scanf("%d", &h);
+            printf("Enter minutes: ");
+            scanf("%d", &m);
+            itoa(h,th,10);
+            itoa(m,tm,10);
+            strcat(th,":"); strcat(th,tm);
+        	for(i=0;i<strlen(th);i++){
+        		fl.time[i]=th[i];
+			}
+        	//printf("%s",fl.time);
+			fl.seats = 5;
+            printf("\nid = %s",fl.id);
+			printf("\nseats = %d",fl.seats);
+			printf("\ntime = %s",fl.time);
+			printf("\nsource = %s",fl.source);
+			printf("\ndestination = %s",fl.destination);
+			printf("\nname = %s",fl.name);
+			//printf("\n%s",fl.id);
+			fwrite(&fl,sizeof(flight),1,file);
+			// fclose(file);
+			break;
+		case 2: // delete flights
+			break;
+		case 3: // modify flight
+            scanf("%d",id);
+            while(fread(&fl,sizeof(flight),1,file)==1){
+                if (strcmp(fl.id,id)==0){
+                    printf("Flight Details.\n");
+                    printf("\nid: %s",fl.id);
+                    printf("\nseats: %d",fl.seats);
+                    printf("\ntime: %s",fl.time);
+                    printf("\nsource: %s",fl.source);
+                    printf("\ndestination: %s",fl.destination);
+                    printf("\nname: %s",fl.name);
+                }else{
+                    printf("Error");
+                }
+            }
+           break;
+		}
+        fclose(file);
+	}
+    
 
 }
 
-void search();
+// void search();
 
-void cancel();
+// void cancel();
 
 int main(){
     system("cls");
